91ffd08dfa55c651a9de778f373ea00d4a1a7fbc
denis-ttk-1975 <denis.ttk@gmail.com>
2024-05-27 21:57:10 +0300

    в TargetComponent примитивный пример useRef

--- src/components/TargetComponent/TargetComponent.tsx
+++ src/components/TargetComponent/TargetComponent.tsx
@@ -10,6 +10,66 @@ export type TargetComponentProps = {
   callback?: () => void;
 };
 
+// const ref = {
+//   current: '',
+// };
+
+// let outSideCallback = (() => {}) as () => unknown;
+// let _deps: unknown[];
+
+// const _useCallback = (callback: () => unknown, deps: unknown[]) => {
+//   if (JSON.stringify(deps) === JSON.stringify(_deps)) return outSideCallback;
+//   _deps = deps;
+//   outSideCallback = callback;
+//   return outSideCallback;
+// };
+
+// const Component = (a: string) => {
+//   ref.current = a;
+//   const func1 = () => {
+//     console.log(ref.current);
+//   };
+//   const func = _useCallback(func1, []);
+
+//   console.log(func === func1);
+
+//   func();
+// };
+
+// Component('1');
+// Component('s');
+// Component('232');
+
+// const ref = {
+//     current: '',
+//   };
+
+//   let outSideCallback = (() => {}) as () => unknown;
+//   let _deps: unknown[];
+
+//   const _useCallback = (callback: () => unknown, deps: unknown[]) => {
+//     if (JSON.stringify(deps) === JSON.stringify(_deps)) return outSideCallback;
+//     _deps = deps;
+//     outSideCallback = callback;
+//     return outSideCallback;
+//   };
+
+//   const Component = (a: string) => {
+//     ref.current = a;
+//     const func1 = () => {
+//       console.log(a);
+//     };
+//     const func = _useCallback(func1, []);
+
+//     console.log(func === func1);
+
+//     func();
+//   };
+
+//   Component('1');
+//   Component('s');
+//   Component('232');
+
 export const TargetComponent = memo<TargetComponentProps>(({ className }) => {
   console.log('rerender TargetComponent');
   return <div className={cn(s.root, className)}>TargetComponent</div>;
